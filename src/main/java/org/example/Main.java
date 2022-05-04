b2cc6ae22c4bbabd03d9af4cc3ca3ff9d4618d1d
“phoenixday” <xtsykyn@fi.muni.cz>
2022-05-04 10:19:59 +0200

    more sparql queries

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,6 @@ package org.example;
 
 import org.apache.jena.query.*;
 import org.apache.jena.rdf.model.*;
-import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFDataMgr;
 
 import java.io.InputStream;
@@ -23,8 +22,10 @@ public class Main {
         //RDFDataMgr.write(System.out, model, Lang.RDFJSON);
         //model.write(System.out);
 
-        System.out.println("\n======= Select all resources for books =======");
-        queryModel(model);
+        selectAllResourcesForBooks(model);
+        selectAllDistinctAuthors(model);
+        selectBooksWrittenAfter2015(model);
+        selectBooksWithLanguageIfExists(model);
     }
 
     /**
@@ -32,11 +33,11 @@ public class Main {
      * @param model RDF data already stored in triplstore
      */
     static void iterate(Model model) {
+        System.out.println("======= Write all triples stored from RDF (rdf.rdf) =======");
         // list the statements in the Model
         StmtIterator iter = model.listStatements();
         // print out the predicate, subject and object of each statement
         while (iter.hasNext()) {
-            System.out.println("=======");
             Statement stmt = iter.nextStatement();  // get next statement
             Resource subject = stmt.getSubject();     // get the subject
             Property predicate = stmt.getPredicate();   // get the predicate
@@ -51,14 +52,15 @@ public class Main {
                 // object is a literal
                 System.out.println(object.toString() + " (literal)");
             }
+            System.out.println();
         }
     }
 
     /**
-     * Example SPARQL query
      * @param model RDF data already stored in triplstore
      */
-    static void queryModel(Model model) {
+    static void selectAllResourcesForBooks(Model model) {
+        System.out.println("\n======= Select all resources for books =======");
         String queryString = "" +
                 "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>" +
                 "PREFIX dc: <http://purl.org/dc/elements/1.1/>" +
@@ -70,10 +72,73 @@ public class Main {
             ResultSet results = qexec.execSelect();
             while (results.hasNext()) {
                 QuerySolution soln = results.nextSolution();
-                //Literal author = soln.getLiteral("author");
-                //System.out.println(author);
                 Resource book = soln.getResource("book");
                 System.out.println(book);
+            }
+        }
+    }
+
+    static void selectAllDistinctAuthors(Model model) {
+        System.out.println("\n======= Select all distinct authors =======");
+        String queryString = "" +
+                "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>" +
+                "PREFIX dc: <http://purl.org/dc/elements/1.1/>" +
+                "SELECT DISTINCT ?author WHERE {" +
+                "   { ?book dc:creator ?bag ." +
+                "   ?bag ?prop ?author ." +
+                "   FILTER (strstarts(str(?prop), str(rdf:_))) }" +
+                "   UNION " +
+                "   { ?book dc:creator ?author ." +
+                "   FILTER (isLiteral(?author)) }" +
+                "}";
+        Query query = QueryFactory.create(queryString);
+        try (QueryExecution qexec = QueryExecutionFactory.create(query, model)) {
+            ResultSet results = qexec.execSelect();
+            while (results.hasNext()) {
+                QuerySolution soln = results.nextSolution();
+                Literal author = soln.getLiteral("author");
+                System.out.println(author);
+            }
+        }
+    }
+
+    static void selectBooksWrittenAfter2015(Model model) {
+        System.out.println("\n======= Select all books writen after >= 2015 =======");
+        String queryString = "" +
+                "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>" +
+                "PREFIX dc: <http://purl.org/dc/elements/1.1/>" +
+                "SELECT * WHERE {" +
+                "   ?book dc:date ?year ." +
+                "   FILTER (?year >= 2015)" +
+                "}";
+        Query query = QueryFactory.create(queryString);
+        try (QueryExecution qexec = QueryExecutionFactory.create(query, model)) {
+            ResultSet results = qexec.execSelect();
+            while (results.hasNext()) {
+                QuerySolution soln = results.nextSolution();
+                Resource book = soln.getResource("book");
+                Literal year = soln.getLiteral("year");
+                System.out.println("Book: " + book + ", year: " + year.getInt());
+            }
+        }
+    }
+
+    static void selectBooksWithLanguageIfExists(Model model) {
+        System.out.println("\n======= Select all books with language if exists =======");
+        String queryString = "" +
+                "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>" +
+                "PREFIX dc: <http://purl.org/dc/elements/1.1/>" +
+                "SELECT * WHERE {" +
+                "   OPTIONAL {?book dc:language ?language}" +
+                "}";
+        Query query = QueryFactory.create(queryString);
+        try (QueryExecution qexec = QueryExecutionFactory.create(query, model)) {
+            ResultSet results = qexec.execSelect();
+            while (results.hasNext()) {
+                QuerySolution soln = results.nextSolution();
+                Resource book = soln.getResource("book");
+                Literal language = soln.getLiteral("language");
+                System.out.println("Book: " + book + ", language: " + language);
             }
         }
     }
